a2b343bb0ad6f07068015c09aaaed59b269755ed
7Nirmal <ra.nirmalkumar96@gmail.com>
2022-06-14 09:45:52 +0530

    new

--- src/Moviecard.js
+++ src/Moviecard.js
@@ -8,9 +8,9 @@ import InfoIcon from "@mui/icons-material/Info";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
+// import CardMedia from "@mui/material/CardMedia";
+// import Button from "@mui/material/Button";
+// import Typography from "@mui/material/Typography";
 
 export function MovieCard({ movies, id }) {
   const styles = { color: movies.rating > 8 ? "green" : "red" };
